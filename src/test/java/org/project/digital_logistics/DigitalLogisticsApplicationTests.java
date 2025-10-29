ea8ad4c7a39139a434ee67ea09304d1006b4bd13
Yahya afadisse <yahyaafadisse92@gmail.com>
2025-10-29 10:29:34 +0100

    ajouter l'envirenement de project

---
+++ src/test/java/org/project/digital_logistics/DigitalLogisticsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.project.digital_logistics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalLogisticsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
