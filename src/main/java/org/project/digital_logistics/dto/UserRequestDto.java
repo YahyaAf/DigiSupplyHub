e1fd726ca946353d2de34cd8571db1b8b6d94c3f
Yahya afadisse <yahyaafadisse92@gmail.com>
2025-10-30 11:00:27 +0100

    fixed le amelioration pour crud of users et finalisé cette entity

--- src/main/java/org/project/digital_logistics/dto/UserRequestDto.java
+++ src/main/java/org/project/digital_logistics/dto/UserRequestDto.java
@@ -1,63 +1,32 @@
 package org.project.digital_logistics.dto;
 
 import org.project.digital_logistics.model.enums.Role;
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class UserRequestDto {
+
+    @NotBlank(message = "Name is required")
+    @Size(min = 2, max = 100, message = "Name must be between 2 and 100 characters")
     private String name;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email should be valid")
     private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, message = "Password must be at least 6 characters")
     private String password;
+
+    @NotNull(message = "Role is required")
     private Role role;
+
     private Boolean active;
-
-    // Constructors
-    public UserRequestDto() {}
-
-    public UserRequestDto(String name, String email, String password, Role role, Boolean active) {
-        this.name = name;
-        this.email = email;
-        this.password = password;
-        this.role = role;
-        this.active = active;
-    }
-
-    // Getters and Setters
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public Role getRole() {
-        return role;
-    }
-
-    public void setRole(Role role) {
-        this.role = role;
-    }
-
-    public Boolean getActive() {
-        return active;
-    }
-
-    public void setActive(Boolean active) {
-        this.active = active;
-    }
 }
