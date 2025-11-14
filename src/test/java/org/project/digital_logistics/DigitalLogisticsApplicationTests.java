76c5d1dd742d772315d3de8c820917ad8bc34e2e
Yahya afadisse <yahyaafadisse92@gmail.com>
2025-11-14 11:43:09 +0100

    more modifcatrion sur pipeline

--- src/test/java/org/project/digital_logistics/DigitalLogisticsApplicationTests.java
+++ src/test/java/org/project/digital_logistics/DigitalLogisticsApplicationTests.java
@@ -2,8 +2,10 @@ package org.project.digital_logistics;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class DigitalLogisticsApplicationTests {
 
     @Test
